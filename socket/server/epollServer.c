d65b35ae32ee082605783c0e7f97a2238f8fd3a8
XiaoHu <3478856774@qq.com>
2021-12-09 12:51:31 +0800

    add something

--- socket/server/epollServer.c
+++ socket/server/epollServer.c
@@ -1,78 +1,160 @@
 //
 // Created by huxiao on 2021/11/27.
 //
+#include  <unistd.h>
+#include  <sys/types.h>       /* basic system data types */
+#include  <sys/socket.h>      /* basic socket definitions */
+#include  <netinet/in.h>      /* sockaddr_in{} and other Internet defns */
+#include  <arpa/inet.h>       /* inet(3) functions */
+#include <sys/epoll.h> /* epoll function */
+#include <fcntl.h>     /* nonblocking */
+#include <sys/resource.h> /*setrlimit */
 
 #include <stdlib.h>
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
-#include <errno.h>
-#include <sys/socket.h>
-#include <resolv.h>
-#include <sys/epoll.h>
-#include <arpa/inet.h>
-#include <unistd.h>
 
-#define PORT 22
-#define SERVER "127.0.0.1"
-#define MAXBUF 1024
-#define MAX_EPOLL_EVENTS 64
+#define MAXEPOLLSIZE 10000
+#define MAXLINE 10240
+int handle(int connfd);
+int setnonblocking(int sockfd)
+{
+    if (fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFD, 0)|O_NONBLOCK) == -1) {
+        return -1;
+    }
+    return 0;
+}
 
-int main() {
-    int sockfd;
-    struct sockaddr_in dest;
-    char buffer[MAXBUF];
-    struct epoll_event events[MAX_EPOLL_EVENTS];
-    int i, num_ready;
+int main(int argc, char **argv)
+{
+    int  servPort = 8022;
+    int listenq = 1024;
 
-    /*---Open socket for streaming---*/
-    if ( (sockfd = socket(AF_INET, SOCK_STREAM|SOCK_NONBLOCK, 0)) < 0 ) {
-        perror("Socket");
-        exit(errno);
+    int listenfd, connfd, kdpfd, nfds, n, nread, curfds,acceptCount = 0;
+    struct sockaddr_in servaddr, cliaddr;
+    socklen_t socklen = sizeof(struct sockaddr_in);
+    struct epoll_event ev;
+    struct epoll_event events[MAXEPOLLSIZE];
+    struct rlimit rt;
+    char buf[MAXLINE];
+
+    /* 设置每个进程允许打开的最大文件数 */
+    rt.rlim_max = rt.rlim_cur = MAXEPOLLSIZE;
+    if (setrlimit(RLIMIT_NOFILE, &rt) == -1)
+    {
+        perror("setrlimit error");
+        return -1;
     }
 
-    /*---Add socket to epoll---*/
-    int epfd = epoll_create(1);
-    struct epoll_event event;
-    event.events = EPOLLIN; // Cann append "|EPOLLOUT" for write events as well
-    event.data.fd = sockfd;
-    epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &event);
 
-    /*---Initialize server address/port struct---*/
-    bzero(&dest, sizeof(dest));
-    dest.sin_family = AF_INET;
-    dest.sin_port = htons(PORT);
-    if ( inet_pton(AF_INET, SERVER, &dest.sin_addr.s_addr) == 0 ) {
-        perror(SERVER);
-        exit(errno);
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl (INADDR_ANY);
+    servaddr.sin_port = htons (servPort);
+
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listenfd == -1) {
+        perror("can't create socket file");
+        return -1;
     }
 
-    /*---Connect to server---*/
-    if ( connect(sockfd, (struct sockaddr*)&dest, sizeof(dest)) != 0 ) {
-        if(errno != EINPROGRESS) {
-            perror("Connect ");
-            exit(errno);
+    int opt = 1;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    if (setnonblocking(listenfd) < 0) {
+        perror("setnonblock error");
+    }
+
+    if (bind(listenfd, (struct sockaddr *) &servaddr, sizeof(struct sockaddr)) == -1)
+    {
+        perror("bind error");
+        return -1;
+    }
+    if (listen(listenfd, listenq) == -1)
+    {
+        perror("listen error");
+        return -1;
+    }
+    /* 创建 epoll 实例，把监听 socket 加入到 epoll 集合里 */
+    kdpfd = epoll_create(MAXEPOLLSIZE);
+    ev.events = EPOLLIN | EPOLLET;// 设置event为IN事件
+    ev.data.fd = listenfd;
+    if (epoll_ctl(kdpfd, EPOLL_CTL_ADD, listenfd, &ev) < 0)
+    {
+        fprintf(stderr, "epoll set insertion error: fd=%d\n", listenfd);
+        return -1;
+    }
+    curfds = 1;
+
+    printf("epollserver startup,port %d, max connection is %d, backlog is %d\n", servPort, MAXEPOLLSIZE, listenq);
+
+    for (;;) {
+        /* 等待有事件发生 */
+        nfds = epoll_wait(kdpfd, events, curfds, -1);
+        if (nfds == -1)
+        {
+            perror("epoll_wait");
+            continue;
+        }
+        /* 处理所有事件 */
+        for (n = 0; n < nfds; ++n)
+        {
+            if (events[n].data.fd == listenfd)
+            {
+                connfd = accept(listenfd, (struct sockaddr *)&cliaddr,&socklen);
+                if (connfd < 0)
+                {
+                    perror("accept error");
+                    continue;
+                }
+
+                sprintf(buf, "accept form %s:%d\n", inet_ntoa(cliaddr.sin_addr), cliaddr.sin_port);
+                printf("%d:%s", ++acceptCount, buf);
+
+                if (curfds >= MAXEPOLLSIZE) {
+                    fprintf(stderr, "too many connection, more than %d\n", MAXEPOLLSIZE);
+                    close(connfd);
+                    continue;
+                }
+                if (setnonblocking(connfd) < 0) {
+                    perror("setnonblocking error");
+                }
+                ev.events = EPOLLIN | EPOLLET;
+                ev.data.fd = connfd;
+                if (epoll_ctl(kdpfd, EPOLL_CTL_ADD, connfd, &ev) < 0)
+                {
+                    fprintf(stderr, "add socket '%d' to epoll failed: %s\n", connfd, strerror(errno));
+                    return -1;
+                }
+                curfds++;
+                continue;
+            }
+            // 处理客户端请求
+            if (handle(events[n].data.fd) < 0) {
+                epoll_ctl(kdpfd, EPOLL_CTL_DEL, events[n].data.fd,&ev);
+                curfds--;
+            }
         }
     }
+    close(listenfd);
+    return 0;
+}
+int handle(int connfd) {
+    int nread;
+    char buf[MAXLINE];
+    nread = read(connfd, buf, MAXLINE);//读取客户端socket流
 
-    /*---Wait for socket connect to complete---*/
-    num_ready = epoll_wait(epfd, events, MAX_EPOLL_EVENTS, 1000/*timeout*/);
-    for(i = 0; i < num_ready; i++) {
-        if(events[i].events & EPOLLIN) {
-            printf("Socket %d connected\n", events[i].data.fd);
-        }
+    if (nread == 0) {
+        printf("client close the connection\n");
+        close(connfd);
+        return -1;
     }
-
-    /*---Wait for data---*/
-    num_ready = epoll_wait(epfd, events, MAX_EPOLL_EVENTS, 1000/*timeout*/);
-    for(i = 0; i < num_ready; i++) {
-        if(events[i].events & EPOLLIN) {
-            printf("Socket %d got some data\n", events[i].data.fd);
-            bzero(buffer, MAXBUF);
-            recv(sockfd, buffer, sizeof(buffer), 0);
-            printf("Received: %s", buffer);
-        }
+    if (nread < 0) {
+        perror("read error");
+        close(connfd);
+        return -1;
     }
-
-    close(sockfd);
+    write(connfd, buf, nread);//响应客户端
     return 0;
 }
